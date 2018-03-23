5f1fd9fba6a062a603c92df26a89340c87838ad6
Tony Macas <iamtonymacas@gmail.comi>
2018-03-23 04:52:12 -0400

    ok

--- assignment.js
+++ assignment.js
@@ -1,1 +1,20 @@
+let sections = document.getElementsByTagName("section")
+let firstSection = sections[0];
+let secondSection = sections[1];
+let thirdSection = sections[2]
 
+//run this code on window scroll
+window.addEventListener("scroll", event => {
+
+  if(window.scrollY > 50) {
+    firstSection.classlist.add("fadeOut");
+  }
+
+  if(sections[1].scrollY > 100) {
+    secondSection.classList.add("fadeOut");
+  }
+
+  if(sections[2].scrollY > 150) {
+    thirdSection.classList.add("fadeOut");
+ }
+})
