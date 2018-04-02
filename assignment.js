c4d80e38db6f0da676e630f61b8aa18056f7c2eb
Tony Macas <iamtonymacas@gmail.comi>
2018-04-02 17:52:50 -0400

    made some comments

--- assignment.js
+++ assignment.js
@@ -1,12 +1,21 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+// "for loop" function
+var slideIndex = 1;//starts with first image
+showDivs(); //calling to display the first image
 
+
+//plusDivs is a function that adds another image to the slideIndex
+//this is the button
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
 
+//this to show the  .div slider-products
 function showDivs(n) {
   var i;
+
+
+  //put my images under class name = mySlides
+  //starts images at index 1 (img/tissue2)
   var x = document.getElementsByClassName("mySlides");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
